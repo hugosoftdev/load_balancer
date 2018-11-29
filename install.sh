fae849f5480cb23d31054b3d7e713855a1584fbc
Hugo Mendes <hugospm@al.insper.edu.br>
2018-11-28 22:33:32 -0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo apt-get update  # To get the latest package lists
+sudo apt-get --assume-yes install python3
+sudo apt-get --assume-yes install python3-pip
+pip3 install boto3
+pip3 install Flask
+python3 run_app.py
